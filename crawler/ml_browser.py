b4201c341451aa5c3e50464d195502a47eddabde
Mairondc21 <duartemairon@gmail.com>
2024-11-17 15:51:28 -0300

    a205mm

--- crawler/ml_browser.py
+++ crawler/ml_browser.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 from bs4 import BeautifulSoup
+import pandas as pd
 
 class BrowserML:
     def __init__(self):
@@ -10,23 +11,42 @@ class BrowserML:
         self.chrome_options.add_argument("--no-sandbox")
         self.chrome_options.add_argument("--disable-web-security")
         self.chrome_options.add_argument("--disable-dev-shm-usage")
-        self.chrome_options.add_argument("--memory-pressure-off")
-        self.chrome_options.add_argument("--ignore-certificate-erros")
+        self.chrome_options.add_argument("--memory_pressure-off")
+        self.chrome_options.add_argument("--ignore-certificate-errors")
 
-        self.drive =webdriver.Chrome(options=self.chrome_options) 
+        self.drive = webdriver.Chrome(options=self.chrome_options) 
 
     def execute_command(self,query):
         self.drive.get(f"https://www.mercadolivre.com.br/{query.replace(' ','-')}")
 
         time.sleep(5)
 
-        html = self.drive.page_source()
+        html = self.drive.page_source
         self.drive.quit()
 
         soup = BeautifulSoup(html, "html.parser")
 
-        results = soup.find_all("div", class_="ui.serach-results")
+        results = soup.find_all("div", class_="ui-search-result")
 
-    def transform_df(self):
-        pass
+        data = []
+
+        for result in results:
+            link = None
+            title = result.find("h2", class_="ui-search-item__title").text.strip()
+            price = result.find("span",class_="andes-money-amount__fraction").text.strip()
+            link_tag = result.find("a",class_="ui-serach-link")
+            if link_tag:
+                link = link_tag.get("href")
+            data.append({"Produto": title, "Preço": price, "URL": link}) 
+
+        return data   
+
+    def transform_df(self,query):
+        data = self.execute_command(query)
+        df = pd.DataFrame(data)
+        return df
+
+crawler = BrowserML()
+dataframe = crawler.transform_df("Playstation")
+print(dataframe)
  
